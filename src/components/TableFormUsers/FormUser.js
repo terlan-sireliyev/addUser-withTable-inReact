a2b3dda5bb01602cc1a1c044ff7e9774d04ea3fd
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2023-12-20 12:16:36 +0300

    addUser-withTable-inReact

--- src/components/TableFormUsers/FormUser.js
+++ src/components/TableFormUsers/FormUser.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 import styleTable from './table.module.css'
 var uniqid = require('uniqid')
-
+// import { tablem, tHead,tbody } from "react-bootstrap";
 export default function FormUser() {
     const [list, setList] = useState([]);
     const [editTodo, setEditTodo] = useState([]);
-    const [name, setName] = useState("");
-    const [age, setAge] = useState("");
-    const [position, setPosition] = useState("");
-    const [phone, setPhone] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,13 +14,8 @@ export default function FormUser() {
         const position = e.target.elements.position.value
         const phone = e.target.elements.phone.value
 
-
         const newList = {
-            id: uniqid(),
-            name,
-            age,
-            position,
-            phone
+            id: uniqid(),name, age, position, phone
         }
         if (name === '' || age === '' || position === '' || phone === '') {
             alert('Inputlar bos ola bilmez')
@@ -32,12 +23,12 @@ export default function FormUser() {
             setList((itemList) => {
                 return itemList.concat(newList)
             })
+            setEditTodo([...editTodo, newList])
         }
-        setName("")
-        setAge("")
-        setPosition("")
-        setPhone("")
-
+         e.target.elements.name.value = ""
+         e.target.elements.age.value = ""
+         e.target.elements.position.value = ""
+         e.target.elements.phone.value = ""
     }
 
     const deleteTab = (id) => {
@@ -45,20 +36,16 @@ export default function FormUser() {
             console.log(itemFil.id + ": " + id)
             return itemFil.id !== id
         })
-
         setList([...delTab])
     }
 
     const editTab = (id) => {
-        const EditTab = list.filter((itemEdit) => {
+       const EditTab = list.filter((itemEdit) => {
             return itemEdit.id === id
         })
         setEditTodo(...EditTab)
     }
-    useEffect(() => {
-        console.log(editTodo.name)
-        console.log(editTodo.position)
-    }, [editTab])
+ 
 
 
     return (
@@ -67,17 +54,12 @@ export default function FormUser() {
             <div className={styleTable.table}>
                 <form onSubmit={handleSubmit}>
                     <div className={styleTable.FormInputbseDiv}>
-                        <input value={editTodo.name} onChange={(e) => setName(e.target.value)} name='name' className={`mt-4 w-25  form-control`} type="text" placeholder='FullName' />
-                        <input value={editTodo.age} onChange={(e) => setAge(e.target.value)} name='age' className={`mt-4 w-25  form-control`} type="text" placeholder='age' />
-                        <input onChange={(e) => setPosition(e.target.value)} name='position' className={`mt-4 w-25  form-control`} type="text" placeholder='Position' />
-                        <input onChange={(e) => setPhone(e.target.value)} name='phone' className={`mt-4 w-25  form-control`} type="text" placeholder='Phone' />
-                        <button
-                            className={`btn ${name === '' ||
-                                age === '' ||
-                                position === '' ||
-                                phone === '' ? "btn-secondary" : "btn-primary"} w-25 mt-2`}
-                            type='submit'
-                        >{editTodo === true ? 'Update' : "Add"}</button>
+                        <input value={editTodo.name}  onChange={(e)=> setEditTodo(e.target.value)}  name='name' className={`mt-4 w-25  form-control`} type="text" placeholder='FullName' />
+                        <input value={editTodo.age}  onChange={(e)=> setEditTodo(e.target.value)}  name='age' className={`mt-4 w-25  form-control`} type="text" placeholder='age' />
+                        <input value={editTodo.position} onChange={(e)=> setEditTodo(e.target.value)} name='position' className={`mt-4 w-25  form-control`} type="text" placeholder='Position' />
+                        <input value={editTodo.phone}  onChange={(e)=> setEditTodo(e.target.value)} name='phone' className={`mt-4 w-25  form-control`} type="text" placeholder='Phone' />
+                        
+                        <button class='btn btn-primary w-25 mt-4'>Add</button>
 
                     </div>
                 </form>
@@ -85,8 +67,7 @@ export default function FormUser() {
                     <span>Sizin bu cedvelin icinde: <span className={`text-danger`}> {list.length} </span> istifadeceniz var</span>
                 </div>
                 <table className={`table table-hover table-bordered mt-1`}>
-                    <thead className=''>
-
+                <thead class="thead-dark">
                         <tr>
                             <th scope="col">FullName</th>
                             <th scope="col">Age</th>
@@ -114,7 +95,9 @@ export default function FormUser() {
                             ))
                         }
                     </tbody>
+            
                 </table>
+                    <input type="text" value={editTodo.name} />
             </div>
         </div>
     )
